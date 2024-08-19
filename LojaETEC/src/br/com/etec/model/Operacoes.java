a0dea3686677c66c1595da956af001db6357c073
lab04 <lab04@lab4-08>
2024-08-19 20:58:12 -0300

    Atualização do Projeto
    -Tela de Login
    > Áreas para colocar o texto Usuário e senha
    > Botões de Validar e Cadastrar
    
    -Lógica de Programação caso os campos do Usuário estejam vazios

--- LojaETEC/src/br/com/etec/model/Operacoes.java
+++ LojaETEC/src/br/com/etec/model/Operacoes.java
@@ -1,5 +1,36 @@
 package br.com.etec.model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Operacoes {
 	
+	@FXML
+	private TextField txfUsuario;
+	@FXML
+	private PasswordField psfUsuario;
+	@FXML
+	private Button btnAcesar;
+	@FXML
+	private Button btnCadastrar;
+	
+	@FXML
+	private void validarUsuario(ActionEvent event) {
+		String nomeUsuario;
+		nomeUsuario = txfUsuario.getText();
+		String senhaUsuario;
+		senhaUsuario = psfUsuario.getText();
+		
+		if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
+			if(nomeUsuario.isEmpty()) {
+				// nome vazio
+			} else {
+				// Senha vazia
+			}
+			
+		}
+	}
 }
